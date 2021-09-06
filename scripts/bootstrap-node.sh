e6edc324424439a9251600f1a89b02664e8a328c
Jhanssen Favaro de Oliveira <jhanssenfavaro@gmail.com>
2021-09-06 16:33:52 -0300

    initial sharing

--- scripts/bootstrap-node.sh
+++ scripts/bootstrap-node.sh
@@ -4,11 +4,15 @@ echo "
 # vagrant environment nodes
 192.168.29.2  kafkalab1
 " > /etc/hosts
-sudo yum install -y centos-release-ansible-29 ansible.noarch ansible-test.noarch git curl
+sudo yum install -y centos-release-ansible-29 python3 git curl
 echo "
 Host *
     StrictHostKeyChecking no
 " > ~/.ssh/config
+pip3 install ansible==2.9.5
+echo "export  $PATH:~/.local/bin" >> ~/.profiles 
+echo "export  $PATH:~/.local/bin" >> ~/.bashrc
+source ~/.bashrc
 sudo chmod 400 ~/.ssh/config
 ssh-keygen -b 2048 -t rsa -f ~/sshkey -q -N ""
 cat ~/sshkey.pub >> ~/.ssh/authorized_keys
