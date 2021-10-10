04d14dbcd37e32f823b2be93b30f46de107be876
Jhanssen Favaro de Oliveira <jhanssen.oliveira@bcb.gov.br>
2021-10-09 22:28:36 -0300

    Correction to VagrantFile and PPT uploaded

--- scripts/bootstrap-node.sh
+++ scripts/bootstrap-node.sh
@@ -3,24 +3,29 @@
 echo "
 # vagrant environment nodes
 192.168.29.2  kafkalab1
+192.168.29.3  kafkalab2
+192.168.29.4  kafkalab3
 " > /etc/hosts
 sudo yum install -y centos-release-ansible-29 python3 git curl
+mkdir ~/.ssh
+touch ~/.ssh/config
 echo "
 Host *
     StrictHostKeyChecking no
 " > ~/.ssh/config
+pip install --upgrade pip
 pip3 install ansible==2.9.5
-echo "export  $PATH:~/.local/bin" >> ~/.profiles 
-echo "export  $PATH:~/.local/bin" >> ~/.bashrc
+echo "export  PATH=\$PATH:/root/.local/bin" >> ~/.profiles 
+echo "export  PATH=\$PATH:/root/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 sudo chmod 400 ~/.ssh/config
 ssh-keygen -b 2048 -t rsa -f ~/sshkey -q -N ""
 cat ~/sshkey.pub >> ~/.ssh/authorized_keys
-ansible --private-key ~/sshkey -m ping -i localhost, all
+# ansible --private-key ~/sshkey -m ping -i localhost, all
 # git clone https://github.com/confluentinc/demo-scene.git
 # git clone https://github.com/confluentinc/examples.git
 # git clone https://github.com/confluentinc/cp-all-in-one.git
 # cd cp-all-in-one/cp-all-in-one
 git clone https://github.com/confluentinc/cp-ansible.git
 cd ~/cp-ansible/
-ansible-playbook --private-key ~/sshkey -i hosts_lab.yml zookeeper.yml
+# ansible-playbook --private-key ~/sshkey -i hosts_lab.yml zookeeper.yml
